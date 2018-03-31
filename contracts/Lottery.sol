d248568b9fec076ea9737113c71a996b6fda899b
shaddu <shadab.ahmed63@gmail.com>
2018-03-31 12:28:49 +0100

    final

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,26 +1,98 @@
 pragma solidity 0.4.20;
 
 contract Lottery {
-    // owner of the lottery
-   address public owner;
 
-   function Lottery() public {
+/** -------- VARIABLES -------- **/
+
+// owner of the lottery
+    address public owner;
+
+// address of the winner
+// assumption only one winner
+    address private winningAddress;
+    address public winner;
+
+// mapping to save token counts for all address 
+    mapping (address => uint) public tokenHolders;
+
+// participant address
+    address[] public participants;
+
+// price of a token
+    uint public token_price;
+
+// game status idicator
+    bool public gameClosed = false;
+
+// Total balance of the smart contract
+    uint public contractBalance;
+
+// winning guess hash
+    bytes32 private winningHash;
+
+/** -------- MODIFIERS -------- **/
+    modifier ownerOnly() {
+            require(msg.sender == owner);
+            _;
+        }
+
+    modifier gameOngoing() {
+            require(!gameClosed);
+            _;
+        }
+    
+    modifier isGameclosed() {
+            require(gameClosed);
+            _;
+        }
+
+   function Lottery(bytes32 winningGuessHash) public {
       owner = msg.sender;
+      token_price = 1.0 ether;
+      gameClosed = false;
+      winningHash = winningGuessHash;
+
    }
 
-   function makeGuess(uint number) public {
+   function buyTokens(address user) public payable gameOngoing {
+
+       if (msg.value / token_price < 1 ) throw;
+        
+        tokenHolders[user] += ~~ (msg.value / token_price ); 
+        // send back the fractions value
+        user.transfer(msg.value - (~~(msg.value / token_price )) * token_price);
+        participants.push(user);
+    
+   }
+
+   function makeGuess(uint guess) public gameOngoing {
+
+       if (tokenHolders[msg.sender] < 1) throw;   
+       // need to change to require
+
+        if (winningHash == sha3(guess)) // need to change to keccak256
+            winningAddress = msg.sender;
+
+        contractBalance += 1;
+        tokenHolders[msg.sender] -= 1;
 
    }
     
-   function winnerAddress() private {
-
+   function winnerAddress() private isGameclosed returns (address) {
+       return winningAddress;
    }
 
-   function getPrice() private {
-
+   function getPrice() public {
+       winner = winnerAddress();
+       if ( winner == msg.sender && contractBalance != 0) {
+            winner.transfer(contractBalance/2);
+            owner.transfer(contractBalance/2);
+            contractBalance = 0;
+       }
    }
 
    function closeGame() private {
-      if(msg.sender == owner) selfdestruct(owner);
+      if (msg.sender == owner) 
+        gameClosed = true;
    }
 }
