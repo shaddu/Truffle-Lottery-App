d89fde91920e652d39aa22e764f94284d3ed7bd7
shaddu <shadab.ahmed63@gmail.com>
2018-03-31 01:29:53 +0100

    contract signature

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,1 +1,26 @@
 pragma solidity 0.4.20;
+
+contract Lottery {
+    // owner of the lottery
+   address public owner;
+
+   function Lottery() public {
+      owner = msg.sender;
+   }
+
+   function makeGuess(uint number) public {
+
+   }
+    
+   function winnerAddress() private {
+
+   }
+
+   function getPrice() private {
+
+   }
+
+   function closeGame() private {
+      if(msg.sender == owner) selfdestruct(owner);
+   }
+}
